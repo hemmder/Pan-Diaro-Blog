0cb3ec7376f085bc01db5c59fd67c196c4457c94
hemmder <hemmder@gmail.com>
2022-04-08 22:30:18 -0400

    Comment main file

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
-    # This will act like a List of BlogPost objects attached to each User.
+    # Relational Database Attributes
     # The "author" refers to the author property in the BlogPost class.
     posts = relationship("BlogPost", back_populates="author")
     comments = relationship ( "Comment" , back_populates = "comment_author" )
@@ -45,16 +45,14 @@ class User(UserMixin, db.Model):
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
-
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    author = relationship("User", back_populates="posts")
-
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
-
+    # Relational Database Attributes
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    author = relationship("User", back_populates="posts")
     comments_post = relationship("Comment", back_populates = "parent_post")
 
 
@@ -62,17 +60,16 @@ class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column ( db.Integer, primary_key = True )
     text = db.Column(db.Text, nullable=False)
-
+    # Relational Database Attributes
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     comment_author = relationship("User", back_populates="comments")
-
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments_post")
 
 
 db.create_all()
 
-# admin-only decorator - Ony admins can edit a created new posts!
+# admin-only decorator - Only admins can edit a created new posts!
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
